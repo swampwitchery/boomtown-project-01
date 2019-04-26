9d985ae301004d70644ae567fe4ea79bef6f5192
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-04-25 20:35:14 -0700

    Working on back end, with postgres

---
+++ boomtown/server/config/apollo.js
@@ -0,0 +1,50 @@
+const { ApolloServer } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = require('../api/schema');
+let resolvers = require('../api/resolvers');
+
+module.exports = ({ app, pgResource }) => {
+  resolvers = resolvers(app);
+
+
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+  });
+
+  const apolloServer = new ApolloServer({
+    context: ({ req }) => {
+      const tokenName = app.get("JWT_COOKIE_NAME")
+      const token = req ? req.cookies[tokenName] : undefined
+
+      return {
+        req,
+        token,
+        pgResource
+        /**
+         * @TODO: Provide Apollo context
+         *
+         * When initializing Apollo, we can provide a context object which will be
+         * passed to each resolver function. This is useful because there are a
+         * number of things we'll need to access in every resolver function.
+         *
+         * Above we can see that we are capturing the cookie from the request object,
+         * and retrieving the token. This is important for authentication.
+         *
+         * Refactor this code and supply any additional information (values, methods, objects...etc)
+         * you'll need to use in your resolving functions.
+         */
+      };
+    },
+    schema
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    // @TODO: Add the CORS_CONFIG from your application configuration
+    cors: undefined
+    // -------------------------------
+  });
+};
---
+++ boomtown/server/config/application.js
@@ -0,0 +1,42 @@
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
+const fallback = require('express-history-api-fallback');
+const path = require('path');
+
+module.exports = (app) => {
+  const PORT = process.env.PORT || 8080;
+
+  app.set('PORT', PORT);
+  app.set('PG_HOST', process.env.PG_HOST || 'localhost');
+  app.set('PG_USER', process.env.PG_USER || 'boomtown');
+  app.set('PG_PASSWORD', process.env.PG_PASSWORD || 'boomtown');
+  app.set('PG_DB', process.env.PG_DB || 'boomtown');
+  app.set('JWT_SECRET', process.env.JWT_SECRET || 'keyboardcat');
+  app.set('JWT_COOKIE_NAME', 'bt-token');
+
+
+  app.use(cookieParser());
+
+  if (process.env.NODE_ENV === 'production') {
+    const root = path.resolve(__dirname, '../public');
+
+    // Serve the static front-end from /public when deployed
+    app.use(express.static(root));
+    app.use(fallback('index.html', { root }));
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    // Allow requests from dev server address
+    const corsConfig = {
+      origin: 'http://localhost:3000',
+      credentials: true
+    };
+    app.set('CORS_CONFIG', corsConfig);
+
+    // Allow requests from dev server address
+    app.use(cors(corsConfig));
+  }
+
+  return PORT;
+};
